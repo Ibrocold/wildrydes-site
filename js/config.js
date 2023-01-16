b35dcbcb1f7f9291fe09de6035856b6939c81137
ibrahim <ibrahimbello349@gmail.com>
2023-01-16 22:32:17 +0000

    Edited config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_auu0CIIDD', // e.g. us-east-1_auu0CIIDD
+        userPoolClientId: 'sudsm02lsdfvpt4dkmr968tar', // e.g. sudsm02lsdfvpt4dkmr968tar
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
